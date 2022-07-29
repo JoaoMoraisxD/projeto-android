432bec2f9126be4f157b9d5ab589130db8a7beb6
Joao Morais <106411312+JoaoMoraisxD@users.noreply.github.com>
2022-07-29 19:47:50 -0300

    js projeto android

---
+++ index.js
@@ -0,0 +1,6 @@
+let imagens = document.querySelectorAll('.small_img')
+let modal = document.querySelector('.modal')
+let modalImg = document.querySelector('#modal_img')
+let btClose = querySelector('#bt_close')
+
+
