3d1b4545603007156d8c96508133ed574f40782b
Joao Morais <106411312+JoaoMoraisxD@users.noreply.github.com>
2022-07-29 21:24:54 -0300

    js projeto android 2
    Atualizando projeto android

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ let imagens = document.querySelectorAll('.small_img')
 let modal = document.querySelector('.modal')
 let modalImg = document.querySelector('#modal_img')
 let btClose = querySelector('#bt_close')
+let srcVal = ""
 
+
+ for(let i = 0; i<imagens.length; i++){
+    imagens[i].addEventListener('click',Function(){
+        
+        srcVal = imagens[i].getAttribute('src')
+        modalImg.setAttribute('src', srcVal)
+        modal.classList.toggle('modal_active')
+    })
+}
+
+btClose.addEventListener('click', Function(){
+    modal.classList.toggle('modal_active')
+})
 
